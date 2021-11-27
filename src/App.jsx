82abb32e21f8c7cb2e843f7988536307041e3f6a
Cristian Garcia <crigar888@gmail.com>
2021-11-27 18:20:26 +0100

    movment done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MapComponent from './components/MapComponent/MapComponent.component';
+
+function App() {
+  return (
+    <div className="App">
+      <MapComponent></MapComponent>
+    </div>
+  );
+}
+
+export default App;
